6a62d9075fec4e709d2e24b089f51e5064ef719c
Yumana <yumanuralfath2@gmail.com>
2023-11-17 08:42:20 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+
+  // eslint-disable-next-line no-console
+  console.log(`server berjalan pada ${server.info.uri}`);
+};
+
+init();
